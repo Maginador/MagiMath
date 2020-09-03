50ff2a1742179a63f1d4698c95f591728b82ef24
Ricardo Costa Maginador <ricardo.cm@SW-RICARDO.local>
2020-09-03 17:47:02 -0400

    Add Vector construction, + and - operators

---
+++ Assets/MagiMath/_Scripts/Vector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MaginaMath{
+    public class Vector
+    {
+
+
+        public float x, y, z;
+        public Vector(float x, float y, float z)
+        {
+
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            var vec = new Vector(a.x + b.x, a.y + b.y, a.z + b.y);
+            return vec;
+        }
+
+        public static Vector operator -(Vector a, Vector b)
+        {
+            var vec = new Vector(a.x - b.x, a.y - b.y, a.z - b.y);
+            return vec;
+        }
+    }
+}
